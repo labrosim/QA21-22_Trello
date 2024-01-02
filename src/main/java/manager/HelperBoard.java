e7e96e76b2a915f50658ea205ecfdaa68b85426d
labrosim <151664560+labrosim@users.noreply.github.com>
2024-01-03 00:20:56 +0200

    added cw

--- src/main/java/manager/HelperBoard.java
+++ src/main/java/manager/HelperBoard.java
@@ -11,6 +11,7 @@ public class HelperBoard extends HelperBase{
     By inputBoardTitle = By.xpath("//input[@data-testid='create-board-title-input']");
     By buttonCreateBoard = By.xpath("//button[@data-testid='create-board-submit-button']");
     By textBoardTitle = By.xpath("//h1[@data-testid='board-name-display']");
+    By buttonBoards = By.xpath("//a[@data-testid='open-boards-link']");
     public void createNewBoard(String boardTitle) {
         clickBase(buttonCreateNewBoard);
         typeBase(inputBoardTitle, boardTitle);
@@ -18,5 +19,22 @@ public class HelperBoard extends HelperBase{
     }
     public boolean isBoardTitlePresents(String text){
         return isTextElementEquals(textBoardTitle,text);
+    }
+
+    public void clickButtonBoards(){
+        clickBase(buttonBoards);
+    }
+
+    public void deleteBoard(String boardTitle) {
+        pause(5);
+        clickBoardsOnTitle(boardTitle);
+    }
+
+    private void clickBoardsOnTitle(String boardTitle) {
+        driver.navigate().refresh();
+      //  String xPathBoardTitle = "//a[@href='/b/hsnwZRdF/"+boardTitle.toLowerCase()+"']";
+        String xPathBoardTitle = "//div[@title='"+boardTitle+"']";
+        System.out.println(xPathBoardTitle);
+        clickBase(By.xpath(xPathBoardTitle));
     }
 }
--- src/test/java/tests/CreateNewBoardTests.java
+++ src/test/java/tests/CreateNewBoardTests.java
@@ -1,24 +1,51 @@
 package tests;
 
+import models.BoardDTO;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Random;
 
-public class CreateNewBoardTests extends TestBase{
+public class CreateNewBoardTests extends TestBase {
 
     @BeforeClass
-    public void login(){
-        app.getHelperUser().login("aksiomamedved@gmail.com","AlexMed123!");
+    public void login() {
+        app.getHelperUser().login("aksiomamedved@gmail.com", "AlexMed123!");
     }
 
 
     @Test
-    public void createNewBoardPositiveTest(){
+    public void createNewBoardPositiveTest() {
         int i = new Random().nextInt(1000) + 1000;
-        String boardTitle = "board"+i;
+        String boardTitle = "board" + i;
         app.getHelperBoard().createNewBoard(boardTitle);
         Assert.assertTrue(app.getHelperBoard().isBoardTitlePresents(boardTitle));
+    }
+
+    @Test
+    public void createNewBoardPositiveTest_Lombok() {
+        int i = new Random().nextInt(1000) + 1000;
+        BoardDTO board = BoardDTO.builder()
+                .boardTitle("boardDTO" + i)
+                .build();
+        app.getHelperBoard().createNewBoard(board.getBoardTitle());
+        Assert.assertTrue(app.getHelperBoard().isBoardTitlePresents(board.getBoardTitle()));
+    }
+    @Test
+    public void deleteBoardPositiveTest(){
+        int i = new Random().nextInt(1000) + 1000;
+        BoardDTO board = BoardDTO.builder()
+                .boardTitle("boardDTO" + i)
+                .build();
+        app.getHelperBoard().createNewBoard(board.getBoardTitle()); //create new board
+        app.getHelperBoard().clickButtonBoards();
+        app.getHelperBoard().deleteBoard(board.getBoardTitle());
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        app.getHelperBoard().clickButtonBoards();
     }
 }
